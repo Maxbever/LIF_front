9bbbefcf382afcc5d3969a3d44b48a3a9d1c3aaa
MaxBever <max.bever98@gmail.com>
2022-06-09 17:18:20 +0200

    Add light spot

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,46 @@ const button_floor2 = document.getElementById('floor2') as HTMLButtonElement;
 const button_floor2Eco = document.getElementById('floor2Eco') as HTMLButtonElement;
 const button_floor3 = document.getElementById('floor3') as HTMLButtonElement;
 const button_floor4 = document.getElementById('floor4') as HTMLButtonElement;
-const body = document.getElementsByTagName('body')[0] as HTMLBodyElement;
+const div_back_ground = document.getElementById('back_ground') as HTMLDivElement;
 
 button_floor0.addEventListener("click",function (){
     removeClass()
-    body.classList.add("floor0")
+    div_back_ground.classList.add("floor0")
 })
 
 button_floor2.addEventListener("click",function (){
     removeClass()
-    body.classList.add("floor2")
+    div_back_ground.classList.add("floor2")
 })
 
 button_floor2Eco.addEventListener("click",function (){
     removeClass()
-    body.classList.add("floor2Eco")
+    div_back_ground.classList.add("floor2Eco")
 })
 
 button_floor3.addEventListener("click",function (){
     removeClass()
-    body.classList.add("floor3")
+    div_back_ground.classList.add("floor3")
 })
 
 button_floor4.addEventListener("click",function (){
     removeClass()
-    body.classList.add("floor4")
+    div_back_ground.classList.add("floor4")
 })
 
 function removeClass(){
-    body.classList.remove("floor2","floor2Eco","floor3","floor4","floor0");
+    div_back_ground.classList.remove("floor2","floor2Eco","floor3","floor4","floor0");
 }
+
+function update(e){
+    var x = e.clientX
+    var y = e.clientY
+
+    div_back_ground.style.setProperty('--cursorX', x + 'px')
+    div_back_ground.style.setProperty('--cursorY', y + 'px')
+}
+
+document.addEventListener('mousemove',update)
+document.addEventListener('touchmove',update)
 
 Data.getData()
